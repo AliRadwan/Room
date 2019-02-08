7774bc53252faf285bd5170703a3a70109080ce1
Ali Radwan <aliradwan116@yahoo.com>
2019-02-09 00:59:29 +0200

    continue

---
+++ app/src/main/java/com/example/wekaradwan/room2/AppDatabase.java
@@ -0,0 +1,10 @@
+package com.example.wekaradwan.room2;
+
+import android.arch.persistence.room.RoomDatabase;
+
+/**
+ * Created by weka radwan on 2/9/2019.
+ */
+
+public abstract class AppDatabase extends RoomDatabase {
+}
